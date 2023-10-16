0d20caabdceb8abc58ffbda15420776e6a633d52
OnurKantarci <148058696+OnurKantarci@users.noreply.github.com>
2023-10-16 13:00:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function changeColor(){
+    const hex_numbers = [
+        "0","1","2","3","4","5","6","7","8","9",
+        "A","B","C","D","E","F"
+    ]
+
+    let hexcode = '';
+
+    for(let i=0; i < 6 ; i++){
+        const random_index = Math.floor(Math.random()
+        * hex_numbers.length);
+
+        hexcode += hex_numbers[random_index]
+    }
+    document.getElementById("hex-code").innerHTML = hexcode;
+    const bodyElements =  document.getElementsByTagName("body");
+    console.log({bodyElements})
+    document.getElementsByTagName("body")[0].style.background = "#" + hexcode;
+
+}
